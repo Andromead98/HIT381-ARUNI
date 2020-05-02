6a5e82fdcad319e66c071cfba12497ff2eee7b69
Silin_Chen <silin.chen05@gmail.com>
2020-05-02 23:22:14 +0930

    Made texts on mostvisited page

--- js/myscript.js
+++ js/myscript.js
@@ -40,3 +40,79 @@ function show_building1() {
   document.getElementById("info__layer").style.display = "none";
   document.getElementById("building_info1").style.display = "block";
 }
+
+
+function show_building2() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info2").style.display = "block";
+}
+
+function show_building3() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info3").style.display = "block";
+}
+
+function show_building4() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info4").style.display = "block";
+}
+function show_building5() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info5").style.display = "block";
+}
+function show_building6() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info6").style.display = "block";
+}
+function show_building7() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info7").style.display = "block";
+}
+function show_building8() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info8").style.display = "block";
+}
+function show_building9() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info9").style.display = "block";
+}
+function show_building10() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info10").style.display = "block";
+}
+function show_building11() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info11").style.display = "block";
+}
+function show_building12() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info12").style.display = "block";
+}
+function show_building13() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info13").style.display = "block";
+}
+function show_building14() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info14").style.display = "block";
+}
+function show_building15() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info15").style.display = "block";
+}
+function show_building16() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info16").style.display = "block";
+}
+function show_building17() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info17").style.display = "block";
+}
+function show_building18() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info18").style.display = "block";
+}
+function show_building19() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("building_info19").style.display = "block";
+}
